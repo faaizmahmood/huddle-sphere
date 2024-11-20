ee1349acf62033b72fd522b0bb1012e9b874d220
Faaiz Mahmood <faizzafar44@gmail.com>
2024-11-20 22:39:07 +0500

    comming soon

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,21 @@
+import './globals.css'
 
 export default function Home() {
+
+  const bg = {
+    backgroundImage: "url(./images/hero_bg.png)",
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  }
+
   return (
     <>
-      Home
+      <section className="comming_soon" style={bg}>
+        <img src='./images/logo.png' alt='Co-Remotely - Co-Working Space in Central Park Lahore' />
+        <h1>Work, Collaborate<span>, Thrive - All in One Place.</span></h1>
+      <button>Comming Soon</button>
+      </section>
     </>
   );
 }
